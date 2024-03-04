4a111bfa829ad0a276088af86108013562be0354
NoOverflow <julien.prost31@ynov.com>
2024-03-04 20:07:18 +0100

    Add Rocket and OpenAPI crates with base laid out

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::form::FromForm;
+use rocket::http::Status;
+use rocket::{get, post, serde::json::Json};
+use rocket_okapi::okapi::schemars;
+use rocket_okapi::okapi::schemars::JsonSchema;
+use rocket_okapi::settings::UrlObject;
+use rocket_okapi::{openapi, openapi_get_routes, swagger_ui::*};
+use serde::{Deserialize, Serialize};
+
+/// # Get if service is ready
+///
+/// 200 if service is ready, used for kubernetes probes
+#[openapi(tag = "Kubernetes Probes")]
+#[get("/readyz")]
+fn readyz_route() -> Status {
+    Status::Ok
+}
+
+#[rocket::main]
+async fn main() {
+    let launch_result = rocket::build()
+        .mount("/api", openapi_get_routes![readyz_route])
+        .mount(
+            "/",
+            make_swagger_ui(&SwaggerUIConfig {
+                url: "/api/openapi.json".to_owned(),
+                ..Default::default()
+            }),
+        )
+        .mount("/", openapi_get_routes![readyz_route])
+        .launch()
+        .await;
+    match launch_result {
+        Ok(_) => println!("Rocket shut down gracefully."),
+        Err(err) => println!("Rocket had an error: {}", err),
+    };
 }
